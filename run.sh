0883324575eb41402e007abe06a674060d3e636e
afogl001 <afogl001@members.fsf.org>
2018-01-22 18:36:44 -0500

    v0.0.2: Non-working checkpoint

--- run.sh
+++ run.sh
@@ -1,57 +1,65 @@
 #!/bin/bash
 
-while : 
+while :
 do
 
 echo "1: Check current Trackpoint settings"
 echo "2: Set Trackpoint settings"
 echo "3: Setup systemd for persistent Trackpoint settings"
 echo "4: Make current Trackpoint settings persistent"
-echo "9: Exit"
+echo "0: Exit"
 echo " "
 read vMainMenu
 
 case $vMainMenu in
-1 ) 
-  printf "Trackpoint Sensitivity: " && cat sensitivity
-  printf "Trackpoint Speed: " && cat speed
-  printf "Trackpoint Press_To_Select: " && cat press_to_select
+1 )
+  printf "Trackpoint Sensitivity: " && cat testing/sys/devices/platform/i8042/serio1/serio2/sensitivity
+  printf "Trackpoint Speed: " && cat testing/sys/devices/platform/i8042/serio1/serio2/speed
+  printf "Trackpoint Press_To_Select: " && cat testing/sys/devices/platform/i8042/serio1/serio2/press_to_select
   echo " "
 ;;
 
 2 )
-  echo "Sensitivity:???"
+  echo "Sensitivity:1-255"
   read vSensitivity
-  echo "Speed:???"
+  echo "Speed:1-255"
   read vSpeed
-  echo "Press_to_Select:?"
+  echo "Press_to_Select:0-1"
   read vPress_to_Select
-  echo $vSensitivity > sensitivity
-  echo $vSpeed > speed
-  echo $vPress_to_Select > press_to_select
+  echo $vSensitivity > testing/sys/devices/platform/i8042/serio1/serio2/sensitivity
+  echo $vSpeed > testing/sys/devices/platform/i8042/serio1/serio2/speed
+  echo $vPress_to_Select > testing/sys/devices/platform/i8042/serio1/serio2/press_to_select
   echo " "
 ;;
 
 3 )
-  cp trackpoint.service /etc/systemd/system
-  cp trackpoint.timer /etc/systemd/system
-  cp trackpoint.sh /usr/bin && chmod +x /usr/bin/trackpoint.sh
-  systemctl start trackpoint
-  systemctl enable trackpoint.timer
-;;
- 
-4 )
-  echo "#!/bin/bash" > trackpoint.sh
-  echo "echo -n $vPress_to_Select > /sys/devices/platform/i8042/serio1/serio2/press_to_select" >> trackpoint.sh
-  echo "echo -n $vSensitivity > /sys/devices/platform/i8042/serio1/serio2/sensitivity" >> trackpoint.sh
-  echo "echo -n $vSpeed > /sys/devices/platform/i8042/serio1/serio2/speed" >> trackpoint.sh
+  cp -r templates/trackpoint.service testing/etc/systemd/system
+  cp -r templates/trackpoint.timer testing/etc/systemd/system
+  cp -r templates/trackpoint.sh testing/usr/bin && chmod +x testing/usr/bin/trackpoint.sh
+  ##Uncomment systemctl commands after test validation
+  #systemctl start trackpoint
+  #systemctl enable trackpoint.timer
 ;;
 
-9 | [eE]xit ) 
+4 )
+if [ ! -z "$vSensitivity" -a ! -z "$vSpeed" -a ! -z "$vPress_to_Select" ];
+then
+  echo "#!/bin/bash" > templates/trackpoint.sh
+  echo "echo -n $vSensitivity > /sys/devices/platform/i8042/serio1/serio2/sensitivity" >> templates/trackpoint.sh
+  echo "echo -n $vSpeed > /sys/devices/platform/i8042/serio1/serio2/speed" >> templates/trackpoint.sh
+  echo "echo -n $vPress_to_Select > /sys/devices/platform/i8042/serio1/serio2/press_to_select" >> templates/trackpoint.sh
+  cp -r templates/trackpoint.sh testing/usr/bin && chmod +x testing/usr/bin/trackpoint.sh
+else
+  echo "Settings not configured.  Please run \"Option 2:\" first"
+  echo ""
+fi
+;;
+
+0 | [eE]xit )
   exit
 ;;
 
-* ) 
+* )
   echo "Pleaes enter a valid option"
   echo " "
 esac
